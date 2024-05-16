7b1bbccd6e80ce4a4a6e643865f1b5ab92164e19
amog1 <160067063+amog1@users.noreply.github.com>
2024-05-16 13:29:04 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+// Magic 8 ball
+// event listner
+document.getElementById("ball").addEventListener("click", reply);
+
+// html variables
+let question = document.getElementById("search");
+let results = document.getElementById("answer");
+let random = Math.random();
+
+// event function
+function reply() {
+  // que and awr
+  console.log(question);
+  if (question.value == "Does a magic ball actually work?") {
+    results.innerHTML = "How dare you doubt me!";
+  } else if (question.value == "Is javaScript awesome?") {
+    results.innerHTML = "Of course!";
+  } else if (question.value == "") {
+    results.innerHTML = "Please ask a question..";
+  } else if (random <= 0.2) {
+    results.innerHTML = "Without a Doubt";
+  } else if (random <= 0.4) {
+    results.innerHTML = "As I see it, yes";
+  } else if (random <= 0.6) {
+    results.innerHTML = "Concentrate and ask again";
+  } else if (random <= 0.8) {
+    results.innerHTML = "Don't count on it";
+  } else {
+    results.innerHTML = "Outlook not so good";
+  }
+}
+
+// event listner
+document.getElementById("reload").addEventListener("click", refresh);
+
+// function
+function refresh() {
+  location.reload();
+}
